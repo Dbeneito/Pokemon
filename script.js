e16f7e3c3bf10f3f83094b2dba164981d33ec78c
Dbeneito <davidbeneitopastor@alumnos.cei.es>
2025-01-14 14:29:43 +0100

    final?

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-const form = document.getElementById
+const form = document.getElementById('pokemon-form');
+const input = documentgetElementById('pokemon-name');
+const pokemonInfoDiv = documentgetElementById('pokemon-info');
+
+async function fetchPokemonData(pokemonName) {
+    try{
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`);
+    
+        if (!response.ok) {
+            throw new Error(`Pokemon no encontrado`);
+        }
+
+        const data = await response.json()
+        displayPokemonInfo(data);
+    } catch (error){
+            pokemonInfoDiv.innerHTML = `<p>${error.message}</p>`;
+            pokemonInfoDiv.style.display = `block`;
+        }
+
+    function displayPokemonInfo(pokemon) {
+        const {name, sprites, types, abilities} = pokemon;
+
+        pokemonInfoDiv.innerHTML =`
+        <img src="${sprites.front_default}" alt="${naeme}">
+        <h3>${name}<h3>
+        <p>Tipo:${types}</p>
+        <p>Habilidades:${abilities}</p>
+        `;
+
+        pokemonInfoDiv.style.display = `block`;
+    }
+
+    form.addEventListener(`submit,`, function(event){
+        event.preventDefault();
+        const pokemonName = input.value.trim();
+        if(pokemonName) {
+            fetchPokemonData(pokemonName);
+        }
+    });
+}
